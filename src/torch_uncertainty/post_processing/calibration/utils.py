a5ad0dc40c3c4fda0446456c1f62b12cb0bcff99
Olivier Laurent <olivier.ar.laurent@gmail.com>
2026-03-10 14:34:23 +0100

    should fail

--- src/torch_uncertainty/post_processing/calibration/utils.py
+++ src/torch_uncertainty/post_processing/calibration/utils.py
@@ -1,5 +1,6 @@
 def _check_classes(num_classes):
     """Check that the number of classes is an int and is striclty positive."""
+    bip boup
     if not isinstance(num_classes, int):
         raise TypeError(f"num_classes must be an integer. Got {num_classes}.")
     if num_classes <= 0:
